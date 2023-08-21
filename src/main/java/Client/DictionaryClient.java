7a110ad1e214eb4f7af88fbd4265734d853c8c96
fschnizzle <fschneider@student.unimelb.edu.au>
2023-08-21 14:00:18 +1000

    Implement basic user request input for client

--- src/main/java/Client/DictionaryClient.java
+++ src/main/java/Client/DictionaryClient.java
@@ -8,7 +8,7 @@ public class DictionaryClient {
     private String hostname;
     private int port;
 
-    /* Constrcutor */
+    /* Constructor */
     public DictionaryClient(String hostname, int port){
         this.hostname = hostname;
         this.port = port;
@@ -17,22 +17,94 @@ public class DictionaryClient {
     /* Methods */
     public void start(){
         try {
+            // Bind client to new socket on host port
             Socket socket = new Socket(hostname, port);
             System.out.println("Connected to server on " + hostname + ":" + port);
 
+            // Open data IO streams
+            DataOutputStream socketOutput = new DataOutputStream(socket.getOutputStream());
+            DataInputStream socketInput = new DataInputStream(socket.getInputStream());
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
             /* Placeholder connection testing functionality */
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            writer.write("Hello, DictionaryServer!");
-            writer.newLine();
-            writer.flush();
+            while (true){
 
-            System.out.println("Message sent to server.");
+                // Prompts user for action
+                int choice = displayMenu(userInput);
+                String request = handleChoice(choice, userInput);
 
-            writer.close();
-            socket.close();
+                // Handles EXIT
+                if (request.equals("EXIT")){
+                    socket.close();
+                    System.exit(0);
+                }
+                // Send Request to server
+
+                // Await response
+            }
+
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Connection Refused: Check port and hostname");
+//            e.printStackTrace();
         }
+    }
+
+    // Replace displayMenu with the visual GUI version later
+    private int displayMenu(BufferedReader userInput) throws IOException {
+        System.out.println("Choose an action:");
+        System.out.println("1. QUERY");
+        System.out.println("2. ADD");
+        System.out.println("3. UPDATE");
+        System.out.println("4. REMOVE");
+        System.out.println("5. EXIT");
+        System.out.print("Choice: ");
+        return Integer.parseInt(userInput.readLine());
+    }
+
+    private String handleChoice(int choice, BufferedReader userInput) throws IOException{
+        String command;
+        String word = "";
+        String meaning = "";
+        switch (choice) {
+            case 1: // QUERY
+                command = "QUERY";
+                word = promptWord("queried", userInput); // Prompts user for text input: "Which word should be queried: "
+                break;
+            case 2: // ADD
+                command = "ADD";
+                word = promptWord("added to the dictionary", userInput); // Prompts user for text input: "Which word should be added to the dictionary: "
+                meaning = promptWordMeaning(word, userInput); // Prompts user for text input: "Enter definition for {word}:"
+                break;
+            case 3: // UPDATE
+                command = "UPDATE";
+                word = promptWord("updated", userInput); // Prompts user for text input: "Which word should be updated: "
+                meaning = promptWordMeaning(word, userInput); // Prompts user for text input: "Enter definition for {word}:"
+                break;
+            case 4: // REMOVE
+                command = "REMOVE";
+                word = promptWord("removed from the dictionary", userInput); // Prompts user for text input: "Which word should be removed from the dictionary"
+                break;
+            case 5: // EXIT
+                command = "EXIT";
+                System.out.println("Exiting...");
+                return "EXIT";
+            default:
+                System.out.println("Invalid choice.");
+                return "";
+        }
+
+        return command + ":" + word + (meaning.isEmpty() ? "" : ":" + meaning);
+
+    }
+
+    private String promptWord(String action, BufferedReader userInput) throws IOException {
+        System.out.println("Which word should be " + action + "?");
+        return userInput.readLine();
+    }
+
+    private String promptWordMeaning(String word, BufferedReader userInput) throws IOException {
+        System.out.println("Enter definition for " + word + ":");
+        return userInput.readLine();
     }
     public static void main(String[] args){
         // Takes command line call like this: java –jar DictionaryClient.jar <server-address> <server-port>
